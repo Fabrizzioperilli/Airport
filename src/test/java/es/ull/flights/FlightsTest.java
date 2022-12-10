ae652a3024916825aca2619204d63ea4332d9c2f
Fabrizzioperilli <alu0101138589@ull.edu.es>
2022-12-10 22:06:04 +0000

    Nuevos Tests para FlightsTest.java

--- src/test/java/es/ull/flights/FlightsTest.java
+++ src/test/java/es/ull/flights/FlightsTest.java
@@ -1,1 +1,56 @@
+package es.ull.flights;
 
+import org.junit.jupiter.api.*;
+
+import es.ull.passengers.Passenger;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FlightsTest {
+
+  Flight flight;
+
+  @BeforeEach
+  void setUp() {
+    flight = new Flight("AA123", 50);
+  }
+
+  @Test
+  @DisplayName("Test flight number")
+  public void testFlightNumber() {
+    assertEquals("AA123", flight.getFlightNumber());
+    assertNotEquals(null, flight.getFlightNumber());
+  }
+
+  @Test
+  @DisplayName("Test flight number with invalid format")
+  public void testFlightNumberInvalidFormat() {
+    assertThrows(RuntimeException.class, () -> new Flight("AA123456", 100));
+  }
+
+  @Test
+  @DisplayName("Test add passenger")
+  public void testAddPassenger() {
+    Passenger passenger = new Passenger("12345678A", "John Doe", "ES");
+    assertTrue(flight.addPassenger(passenger));
+  }
+
+  @Test
+  @DisplayName("Test add passenger with less seats")
+  public void testAddPassengerWithLessSeats() {
+    Flight flight = new Flight("AA123", 1);
+    Passenger passenger = new Passenger("12345678A", "John Doe", "ES");
+    flight.addPassenger(passenger);
+    assertThrows(RuntimeException.class, () -> flight.addPassenger(passenger));
+  }
+
+  @Test
+  @DisplayName("Test remove passenger")
+  public void testRemovePassenger() {
+    Passenger passenger = new Passenger("12345678A", "John Doe", "ES");
+    flight.addPassenger(passenger);
+    assertTrue(flight.removePassenger(passenger));
+  }
+};
